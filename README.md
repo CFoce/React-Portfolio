e7551be0c2709e46feba9ec455ceb679602f3fe3
CFoce <cfoce.dev@gmail.com>
2023-08-08 12:09:19 -0400

    Init Commit
    Initial Commit at project creation

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-portfolio
+ Front End of Portfolio Site
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+ 
+ function Message() {
+    const name = 'Chris Foce';
+    return <h1>{name}</h1>;
+ }
+
+ export default Message;
